386d8d72486d8184fc09633ab22d7d6322504f7e
teutzue <teutzue_1995@yahoo.com>
2015-03-03 10:13:42 +0100

    Deleted duplicates in the main class

--- GitTestMartin/src/gittestmartin/GitTestMartin.java
+++ GitTestMartin/src/gittestmartin/GitTestMartin.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,38 +28,4 @@ public class GitTestMartin {
         System.out.println("Did you know that seagulls will stomp repeatedly on the grass"
                 + " inorder to fool the worms into thinking that its raining?");
     }
-    
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gittestmartin;
-
-/**
- *
- * @author CosticaTeodor
- */
-public class GitTestMartin {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //Test git Teo :P
-        //Test Martin
-        Class c = new Class();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hi Martin, it works!");
-            System.out.println("dsfgjkulikgjfhhjklk.h,cngm");
-        }
-        c.Great();
-        
-    }
-    
-}
->>>>>>> 0ba0f99cf4d493f3b64b1414083e042c8b730a93
